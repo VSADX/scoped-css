da29bb369de01fc18d4f978ac080d244810d7985
VSADX <81491645+VSADX@users.noreply.github.com>
2021-06-26 19:43:47 -0500

    Update scoped-css.js

--- scoped-css.js
+++ scoped-css.js
@@ -1,9 +1,19 @@
-export function generate_scoped_styles(doc = document) {
+export function generate_scoped_styles(doc = document, use_class_name = false) {
     doc.querySelectorAll("style.scoped").forEach(style => {
         const parent_el = style.parentElement
-        parent_el.id = parent_el.id || "i"+(Math.random() * 1e9 | 0).toString(16)
+        const rand = (Math.random() * 1e9 | 0).toString(16)
 
-        style.innerHTML = style.innerHTML.split("\\\&").join("#"+parent_el.id)
+        if(use_class_name) {
+            var id = `c${rand}`
+            parent_el.classList.add(id)
+            id = `.${id}`
+        }
+        else {        
+            parent_el.id = parent_el.id || `i${rand}`
+            var id = `#${parent_el.id}`
+        }
+
+        style.innerHTML = style.innerHTML.split("\\\&").join(id)
     })
 }
 generate_scoped_styles()
